aa55f0620b5381736217e713a1b59ec7917f6ae1
dipran11 <158012785+dipran11@users.noreply.github.com>
2024-07-24 22:27:44 +0700

    Update EndPoint.java

--- src/test/java/helper/EndPoint.java
+++ src/test/java/helper/EndPoint.java
@@ -1,2 +1,13 @@
-package helper;public class EndPoint {
+package helper;
+
+public class EndPoint {
+
+    public  static final String host_gorest = "https://gorest.co.in/public/v2/";
+
+    public static final String GET_LIST_USER = host_gorest + "users";
+    public static final String CREATE_NEW_USER = host_gorest + "users";
+    public static final String DELETE_USER = host_gorest + "users";
+
+
+
 }
