3a7da0cab9b1d2f0612eae0af1e0ba5b70b4bc52
dipran11 <158012785+dipran11@users.noreply.github.com>
2024-07-24 22:28:53 +0700

    Update ApiRunner.java

--- src/test/java/runner/ApiRunner.java
+++ src/test/java/runner/ApiRunner.java
@@ -1,2 +1,17 @@
-package runner;public class ApiRunner {
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        glue = {"stepDef"},
+        features = {"src/test/java/features"},
+        tags ="@api",
+        monochrome = true
+)
+public class ApiRunner {
 }
