89fb2d308cd3883df22254504ba761f1528de622
dipran11 <158012785+dipran11@users.noreply.github.com>
2024-07-24 22:28:08 +0700

    Update Models.java

--- src/test/java/helper/Models.java
+++ src/test/java/helper/Models.java
@@ -1,2 +1,40 @@
-package helper;public class Models {
+package helper;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
+
+public class Models {
+
+    private static RequestSpecification request;
+
+    public static void  setupHeaders() {
+        request = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .header("Authorization", "Bearer 3de495d658779815098d244a0f3c62d4f58877cbc2693758b23f77838270b29c");
+    }
+
+    public static Response getListUsers(String endpoint) {
+        setupHeaders();
+        return request.when().get(endpoint);
+    }
+
+    public static Response postCreateUser(String endpoint) {
+        String name = "Brock love";
+        String gender = "male";
+        String email = "allasani.peddana@15ce.com";
+        String status = "active";
+        JSONObject payload = new JSONObject();
+        payload.put("name", name);
+        payload.put("gender", gender);
+        payload.put("email", email);
+        payload.put("status", status);
+
+        setupHeaders();
+        return request.body(payload.toString()).when().post();
+    }
 }
+
+
